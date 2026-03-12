ceb659a08c6e6d9255c9bc5161767592740e268a
kiiisy <programming.ksy.2@gmail.com>
2026-03-13 00:42:10 +0900

    test: github actionsを登録

---
+++ .config/fpga/verilator.f
@@ -0,0 +1,8 @@
+src/myip/arec_1_0/hdl/bram_ctrl.v
+src/myip/arec_1_0/hdl/core_ctrl.v
+src/myip/arec_1_0/hdl/core.v
+src/myip/arec_1_0/hdl/data2stream.v
+src/myip/arec_1_0/hdl/rd_ctrl.v
+src/myip/arec_1_0/hdl/stream2data.v
+src/myip/arec_1_0/hdl/window_detector.v
+src/myip/arec_1_0/hdl/wr_ctrl.v
